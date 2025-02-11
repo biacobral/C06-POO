39d18fb62dda7023a6410096c67a5681346ee584
bia <95378263+biacobral@users.noreply.github.com>
2025-02-11 13:32:03 -0300

    Add files via upload

---
+++ Aulas/Lanches/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        float soma = lanche1 + lanche2 + lanche3;
+
+        float media = soma / 3;
+
+        System.out.println("Total de lanches: " + soma);
+        //System.out.println("Total de lanches: " + (lanche1 + lanche2 + lanche3));
+
+        System.out.println("Média de lanches: " + media);
+
+    }
+}
